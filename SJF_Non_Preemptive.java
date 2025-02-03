12c2317610fede69895786560c4ed20d6741b2ac
Utkal Behera <utkalbehera59@gmail.com>
2025-02-04 00:22:42 +0530

    Initial commit: here add the code input, sorting at accordingly others at last print the results

---
+++ SJF_Non_Preemptive.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class SJF_Non_Preemptive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+        
+        int Process_id[] = new int[n];
+        int Arrival_time[] = new int[n];
+        int Burst_time[] = new int[n];
+        int Compilation_time[] = new int[n];
+        int Turn_arround_time[] = new int[n];
+        int Wating_time[] = new int[n];
+        int Response_time[] = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            Process_id[i] = i + 1;
+            System.out.print("Enter arrival time of P" + Process_id[i] + ": ");
+            Arrival_time[i] = sc.nextInt();
+            System.out.print("Enter burst time of P" + Process_id[i] + ": ");
+            Burst_time[i] = sc.nextInt();
+        }
+
+        // Sort processes based on arrival time first
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Arrival_time[i] > Arrival_time[j]) {
+                    swap(Arrival_time, i, j);
+                    swap(Burst_time, i, j);
+                    swap(Process_id, i, j);
+                }
+            }
+        }
+
+        // Print results
+        System.out.println("\nProcess\tAT\tBT\tCT\tTAT\tWT\tRT");
+        for (int i = 0; i < n; i++) {
+            System.out.println("P" + Process_id[i] + "\t" + Arrival_time[i] + "\t" + Burst_time[i] + "\t" + Compilation_time[i] + "\t" + Turn_arround_time[i] + "\t" + Wating_time[i] + "\t" + Response_time[i]);
+        }
+
+        sc.close();
+    }
+
+    // Swap function for sorting
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
