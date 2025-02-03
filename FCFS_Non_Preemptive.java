b5f3a5c142c3373dcd304c37be71ffccd78aa54e
Utkal Behera <utkalbehera59@gmail.com>
2025-02-03 23:33:42 +0530

    Initial commit: Added FCFS Non-Preemptive scheduling algorithm and just calculate CT for checking it's comes as right or wrong

---
+++ FCFS_Non_Preemptive.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class FCFS_Non_Preemptive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+        
+        int Busr_time[] = new int[n];
+        
+        int Process_id[] = new String[n];
+        int Arrival_time[] = new int[n];
+        int Burst_time[] = new int[n];
+        int Compilation_time[] = new int[n];
+        int Turn_arround_time[] = new int[n];
+        int Wating_time[] = new int[n];
+        int Response_time[] = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            Process_id[i] = i + 1;
+            System.out.print("Enter arrival time of P" + Process_id[i] + ": ");
+            Arrival_time[i] = sc.nextInt();
+            System.out.print("Enter burst time of P" + Process_id[i] + ": ");
+            Busr_time[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (Arrival_time[i] > Arrival_time[j]) {
+                    swap(Arrival_time, i, j);
+                    swap(Burst_time, i, j);
+                    swap(Process_id, i, j);
+                }
+            }
+        }
+
+        int currentTime = 0;
+        for (int i = 0; i < n + 1; i++) {
+            Compilation_time[i] = currentTime + Burst_time[i];
+            currentTime = Compilation_time[i];
+        }
+
+        sc.close();
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
